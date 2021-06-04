dde2c79f6c1ef816453c68f65e8a76c1a16d8c00
Harika-1216 <82093814+Harika-1216@users.noreply.github.com>
2021-06-04 15:08:25 +0530

    Delete feedbackSystemAngular directory

--- feedbackSystemAngular/src/app/classes/course.ts
+++
@@ -1,7 +0,0 @@
-export class Course {
-    courseId:number| undefined; 
-	courseName:string | undefined; 
-	courseDescription:string | undefined;
-	noOfDays:number| undefined;
-    constructor(courseId:number,courseName:string,courseDescription:string,noOfDays:number){}
-}
